3ce116cf7ed45a207b19a647fc45c91263fa46b2
strfry <mail@strfry.org>
2020-01-22 01:15:37 +0100

    remove cmake stuff and get things working for r21

--- cross/build-armv7a.sh
+++ cross/build-armv7a.sh
@@ -7,8 +7,8 @@ ABI="armeabi-v7a"
 API_LEVEL="21"
 BUILD_DIR="armv7a-build"
 
-ANDROID_SDK_HOME="/home/ppetraki/Android/Sdk"
-ANDROID_NDK_HOME="${ANDROID_SDK_HOME}/ndk-bundle"
+ANDROID_SDK_HOME="/opt/android"
+ANDROID_NDK_HOME="${ANDROID_SDK_HOME}/android-ndk-r21"
 TOOLCHAIN="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake"
 
 HOST_ROOT="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64"
@@ -17,12 +17,6 @@ LIB_PATH="/${SYS_ROOT}/usr/lib/${ARCH}-linux-androideabi:${SYS_ROOT}/usr/lib/${A
 INC_PATH="${SYS_ROOT}/usr/include"
 
 export PATH="${HOST_ROOT}/bin:${PATH}"
-
-# XXX I not sure how much of this cmake config is actually having an impact
-export CMAKE_PREFIX_PATH="${SYS_ROOT}"
-export CMAKE_ROOT="${ANDROID_SDK_HOME}/Android/Sdk/cmake/3.10.2.4988404"
-export CMAKE_LIBRARY_PATH=${LIB_PATH}
-export CMAKE_INCLUDE_PATH=${INC_PATH}
 
 export CFLAGS="-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN} \
   -DANDROID_STL=c++_shared \
@@ -37,11 +31,12 @@ echo "Toolchain: ${TOOLCHAIN}"
 rm -rf ${BUILD_DIR}
 
 meson setup --errorlogs  \
+  --cross-file cross/android-armhf.ini  \
   --prefix=${ANDROID_NDK_HOME} \
   --includedir=${INC_PATH} \
   --libdir=${LIB_PATH} \
-  --build.cmake-prefix-path=${SYS_ROOT} \
-  --cross-file cross/android-armhf.ini  \
   ${BUILD_DIR} .
+
+#  --build.cmake-prefix-path=${SYS_ROOT} \
 
 ninja -C ${BUILD_DIR}
